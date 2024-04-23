fe867c36308ae534180ca2c3b5af97ca5540ab15
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2024-04-23 10:02:14 -0300

    Initial commit

---
+++ src/main/java/com/simplifiedpayment/SimplifiedpaymentApplication.java
@@ -0,0 +1,13 @@
+package com.simplifiedpayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplifiedpaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplifiedpaymentApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/simplifiedpayment/domain/user/User.java
@@ -0,0 +1,17 @@
+package com.simplifiedpayment.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity(name="users")
+@Table(name="users")
+@Getter
+@Setter
+@AllArgsConstructor
+@EqualsAndHashCode(of="id")
+public class User {
+}
