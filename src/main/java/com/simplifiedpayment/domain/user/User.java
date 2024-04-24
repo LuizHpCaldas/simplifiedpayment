6d07d11d19b3511d55924e45220359e8f6dcd198
Luiz Henrique Caldas <luizhpcaldas@gmail.com>
2024-04-24 11:54:02 -0300

    feat: create DTOs

--- src/main/java/com/simplifiedpayment/domain/user/User.java
+++ src/main/java/com/simplifiedpayment/domain/user/User.java
@@ -1,5 +1,6 @@
 package com.simplifiedpayment.domain.user;
 
+import com.simplifiedpayment.dtos.UserDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -26,4 +27,13 @@ public class User {
     private BigDecimal balance;
     @Enumerated(EnumType.STRING)
     private UserType userType;
+
+    public User(UserDTO data){
+        this.firstName = data.firstName();
+        this.lastName = data.lastName();
+        this.balance = data.balance();
+        this.userType = data.userType();
+        this.password = data.password();
+        this.email = data.email();
+    }
 }
